8f38f436cd6f3cce538a15ac3614b577df62b56a
MunimIftikhar <munimiftikhar1@gmail.com>
2024-07-23 15:54:33 +0500

    Update Contracts

--- contracts/ArtworksRegistry.sol
+++ contracts/ArtworksRegistry.sol
@@ -1,23 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "./ArtworkERC721NFT.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract ArtworksRegistry is Ownable {
-        // Declares a private counter for tracking token IDs.
+    using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // struct to manage the Artist's information
     struct Artist {
         string name;
+        string profileURI;
     }
 
-    struct User {
-        string name;
-    }
-
-    // struct to manage the Artwork's information
     struct Artwork {
         uint256 id;
         address artist;
@@ -27,52 +22,43 @@ contract ArtworksRegistry is Ownable {
         bool isNFTMinted;
     }
 
-    // Struct to store Collection details
     struct Collection {
         string name;
         uint256[] artworkIds;
     }
 
-    // Mapping of addresses to artist details
     mapping(address => Artist) public artists;
-    // Mapping of artworks IDs to artworks details
     mapping(uint256 => Artwork) public artworks;
-    // Mapping of addresses and artworks names to collections
     mapping(address => mapping(string => Collection)) public collections;
 
-    // Events to log actions
     event ArtistRegistered(address indexed artistAddress, string name);
     event ArtworkAdded(uint256 indexed artworkId, address indexed artist, string title);
     event ArtworkStreamed(uint256 indexed artworkId, address indexed listener);
     event NFTMinted(uint256 indexed tokenId, address indexed buyer, uint256 price);
     event CollectionCreated(address indexed creator, string name);
-    event ArtworkAddedToCollection(address indexed creator, string CollectionName, uint256 artworkId);
+    event ArtworkAddedToCollection(address indexed creator, string collectionName, uint256 artworkId);
 
-    // Function to register an artist with name and profile URI
     function registerArtist(string memory _name, string memory _profileURI) external {
         require(bytes(artists[msg.sender].name).length == 0, "Artist already registered");
         artists[msg.sender] = Artist(_name, _profileURI);
         emit ArtistRegistered(msg.sender, _name);
     }
 
-    // Function to add a artwork with title, picture URI, and NFT price
     function addArtwork(string memory _title, string memory _pictureURI, uint256 _nftPrice) external {
         require(bytes(artists[msg.sender].name).length > 0, "Artist not registered");
         _tokenIds.increment();
         uint256 newArtworkId = _tokenIds.current();
 
-        artworks[newArtworkId] = Artwork(newartworkId, msg.sender, _title, _pictureURI, 0, _nftPrice, false);
+        artworks[newArtworkId] = Artwork(newArtworkId, msg.sender, _title, _pictureURI, _nftPrice, false);
         emit ArtworkAdded(newArtworkId, msg.sender, _title);
     }
 
-    // Function to create a collection with a given name
     function createCollection(string memory _name) external {
         require(collections[msg.sender][_name].artworkIds.length == 0, "Collection already exists");
-        _artworkId[msg.sender][_name] = Collection(_name, new uint256[](0));
+        collections[msg.sender][_name] = Collection(_name, new uint256[](0));
         emit CollectionCreated(msg.sender, _name);
     }
 
-    // Function to add a artwork to an existing collection
     function addArtworkToCollection(string memory _collectionName, uint256 _artworkId) external {
         require(artworks[_artworkId].id != 0, "Artwork does not exist");
         require(collections[msg.sender][_collectionName].artworkIds.length > 0, "Collection does not exist");
@@ -80,15 +66,12 @@ contract ArtworksRegistry is Ownable {
         emit ArtworkAddedToCollection(msg.sender, _collectionName, _artworkId);
     }
 
-    // Function to get the artwork IDs in a user's collection
     function getCollection(address _user, string memory _collectionName) external view returns (uint256[] memory) {
         return collections[_user][_collectionName].artworkIds;
     }
 
-    // Function to get the details of a specific artwork
-    function getArtworkDetails(uint256 _artworkId) external view returns (artwork memory) {
+    function getArtworkDetails(uint256 _artworkId) external view returns (Artwork memory) {
         require(artworks[_artworkId].id != 0, "Artwork does not exist");
         return artworks[_artworkId];
     }
 }
-
--- contracts/NFTMarketPlace.sol
+++ contracts/NFTMarketPlace.sol
@@ -3,50 +3,85 @@ pragma solidity ^0.8.4;
 
 import "./ArtworkERC721NFT.sol";
 import "./ArtworkERC1155NFT.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./ArtworksRegistry.sol";
+
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract NftMarketplace is ReentrancyGuard {
 
+contract NFTMarketplace is ReentrancyGuard {
     struct Collection {
         address collectionAddress;
         string name;
+        bool isERC1155;
     }
 
     mapping(string => Collection) public collections;
+    ArtworksRegistry public registry;
 
-    event CollectionCreated(string indexed name, address indexed collectionAddress);
+    event CollectionCreated(string indexed name, address indexed collectionAddress, bool isERC1155);
+    event NFTMinted(uint256 indexed tokenId, address indexed collectionAddress, uint256 tokenIdInCollection, bool isERC1155);
+
+    constructor(address _registryAddress) {
+        registry = ArtworksRegistry(_registryAddress);
+    }
 
     function createERC721Collection(string memory name, string memory symbol) public {
         require(collections[name].collectionAddress == address(0), "Collection already exists");
 
         ArtworkERC721NFT newCollection = new ArtworkERC721NFT(name, symbol);
-        collections[name] = Collection(address(newCollection), name);
+        collections[name] = Collection(address(newCollection), name, false);
 
-        emit CollectionCreated(name, address(newCollection));
+        // Register the collection in the registry
+        registry.createCollection(name);
+
+        emit CollectionCreated(name, address(newCollection), false);
     }
 
-    function mintInERC721Collection(string memory name, address tokenURI) public {
-        Collection memory collection = collections[name];
-        require(collection.collectionAddress != address(0), "Collection does not exist");
-
-        ArtworkERC721NFT(collection.collectionAddress).mintNFT(tokenURI);
-    }
-
-    function createERC1155Collection(string memory name, string memory symbol, string memory uri) public {
+    function createArtworkERC1155NFT(string memory name, string memory symbol, string memory uri) public {
         require(collections[name].collectionAddress == address(0), "Collection already exists");
 
         ArtworkERC1155NFT newCollection = new ArtworkERC1155NFT(name, symbol, uri);
         collections[name] = Collection(address(newCollection), name, true);
 
+        // Register the collection in the registry
+        registry.createCollection(name);
+
         emit CollectionCreated(name, address(newCollection), true);
     }
 
-    function mintInERC1155Collection(string memory name, address recipient, uint256 amount, bytes memory data) public {
+    function mintInERC721Collection(string memory name, address recipient, string memory title, string memory pictureURI, uint256 nftPrice) public {
+        Collection memory collection = collections[name];
+        require(collection.collectionAddress != address(0), "Collection does not exist");
+        require(!collection.isERC1155, "Not an ERC721 collection");
+
+        ArtworkERC721NFT ArtworkERC721NFT = ArtworkERC721NFT(collection.collectionAddress);
+        uint256 tokenId = ArtworkERC721NFT.mintNFT(recipient);
+
+        // Add the artwork to the registry
+        registry.addArtwork(title, pictureURI, nftPrice);
+        uint256 artworkId = registry.getArtworkId();
+
+        // Associate the minted NFT with the artwork
+        registry.addArtworkToCollection(name, artworkId);
+
+        emit NFTMinted(tokenId, collection.collectionAddress, tokenId, false);
+    }
+
+    function mintInArtworkERC1155NFT(string memory name, address recipient, uint256 amount, bytes memory data, string memory title, string memory pictureURI, uint256 nftPrice) public {
         Collection memory collection = collections[name];
         require(collection.collectionAddress != address(0), "Collection does not exist");
         require(collection.isERC1155, "Not an ERC1155 collection");
 
-        ArtworkERC1155NFT(collection.collectionAddress).mintNFT(recipient, amount, data);
+        ArtworkERC1155NFT artworkERC1155NFT = ArtworkERC1155NFT(collection.collectionAddress);
+        uint256 tokenId = artworkERC1155NFT.mintNFT(recipient, amount, data);
+
+        // Add the artwork to the registry
+        registry.addArtwork(title, pictureURI, nftPrice);
+        uint256 artworkId = registry.getArtworkId();
+
+        // Associate the minted NFT with the artwork
+        registry.addArtworkToCollection(name, artworkId);
+
+        emit NFTMinted(tokenId, collection.collectionAddress, tokenId, true);
     }
 }
