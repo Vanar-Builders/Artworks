1ebd85b405f8837183d7add68da3fb2c22757c05
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-07-25 00:01:55 +0500

    Added EIP2981 contracts and added EIP to ERC721 and ERC1155 contractsfor royalties

--- contracts/ArtworkERC1155NFT.sol
+++ contracts/ArtworkERC1155NFT.sol
@@ -3,22 +3,63 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./royalties/ERC2981PerTokenRoyalties.sol";
 
-contract ArtworkERC1155NFT is ERC1155, Ownable {
+contract ArtworkERC1155NFT is ERC1155, Ownable, ERC2981PerTokenRoyalties {
     uint256 public tokenCounter;
     string public name;
     string public symbol;
 
-    constructor(string memory _name, string memory _symbol, string memory uri) ERC1155(uri) {
+    /// @inheritdoc	ERC165
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC1155, ERC2981Base)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    constructor(string memory _name, string memory _symbol, string memory uri) ERC1155(uri) Ownable(msg.sender) {
         name = _name;
         symbol = _symbol;
         tokenCounter = 0;
     }
 
-    function mintNFT(address recipient, uint256 amount, bytes memory data) public onlyOwner returns (uint256) {
+    function mintNFT(address recipient, uint256 amount, bytes memory data, uint256 royaltyValue) public onlyOwner returns (uint256) {
         uint256 newTokenId = tokenCounter;
         _mint(recipient, newTokenId, amount, data);
         tokenCounter += 1;
+        if (royaltyValue > 0) {
+            _setTokenRoyalty(newTokenId, recipient, royaltyValue);
+        }
         return newTokenId;
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        address[] memory royaltyRecipients,
+        uint256[] memory royaltyValues
+    ) external {
+        require(
+            ids.length == royaltyRecipients.length &&
+                ids.length == royaltyValues.length,
+            'ERC1155: Arrays length mismatch'
+        );
+
+        _mintBatch(to, ids, amounts, '');
+
+        for (uint256 i; i < ids.length; i++) {
+            if (royaltyValues[i] > 0) {
+                _setTokenRoyalty(
+                    ids[i],
+                    royaltyRecipients[i],
+                    royaltyValues[i]
+                );
+            }
+        }
     }
 }
--- contracts/ArtworkERC721NFT.sol
+++ contracts/ArtworkERC721NFT.sol
@@ -5,14 +5,26 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./royalties/ERC2981ContractWideRoyalties.sol";
 
-contract ArtworkERC721NFT is ERC721URIStorage, Ownable {
+contract ArtworkERC721NFT is ERC721URIStorage, Ownable, ERC2981ContractWideRoyalties {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
     uint256 public constant ROYALTY_PERCENTAGE = 30; // 30% royalty on NFT minting
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    /// @inheritdoc	ERC165
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, ERC2981Base)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
     function mintNFT(string memory _tokenURI) external payable returns (uint) {
         uint256 tokenId = _tokenIdCounter.current();
@@ -32,5 +44,25 @@ contract ArtworkERC721NFT is ERC721URIStorage, Ownable {
         require(success, "Royalty payout failed");
 
         emit RoyaltyPaid(artist, amount);
+    }
+
+    /// @notice Allows to set the royalties on the contract
+    /// @dev This function in a real contract should be protected with a onlyOwner (or equivalent) modifier
+    /// @param recipient the royalties recipient
+    /// @param value royalties value (between 0 and 10000)
+    function setRoyalties(address recipient, uint256 value) public {
+        _setRoyalties(recipient, value);
+    }
+
+    /// @notice Mint several tokens at once
+    /// @param recipients an array of recipients for each token
+    function mintBatch(address[] memory recipients) external {
+        uint256 tokenId = nextTokenId;
+        for (uint256 i; i < recipients.length; i++) {
+            _safeMint(recipients[i], tokenId, '');
+            tokenId++;
+        }
+
+        nextTokenId = tokenId;
     }
 }
