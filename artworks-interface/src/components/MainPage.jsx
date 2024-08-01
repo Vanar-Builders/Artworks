ce4cea6296e5b319f1353f09c11cfadc23d39f17
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-08-01 16:10:49 +0500

    Add Interface

---
+++ artworks-interface/src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import NavBar from "./Navbar";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+        <NavBar />  
+    </div>
+  );
+};
+
+export default MainPage;
