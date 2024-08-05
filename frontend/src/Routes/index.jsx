45cd8a6c43231fc4b4184beccaea2f292eb60ce0
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-08-06 00:30:59 +0500

    single nfts

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 import { CreateNFTS } from "../pages/CreateNft";
+import { CreateSingleNFT } from "../pages/CreateSingleNft";
 
 export const ReactRoutes = () => {
     return(
         <Routes>
             <Route path="/" element={<HeroSection/>}/>
             <Route path="/create" element={<CreateNFTS/>}/>
+            <Route path="/create/single" element={<CreateSingleNFT/>}/>
         </Routes>
     )
 }
