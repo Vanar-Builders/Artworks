dff6916c4c0f130a99831d68d4b16a31b0d1db75
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-08-06 01:42:55 +0500

    create collectionm

--- frontend/src/Routes/index.jsx
+++ frontend/src/Routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 import { CreateNFTS } from "../pages/CreateNft";
 import { CreateSingleNFT } from "../pages/CreateSingleNft";
+import { CreateCollection } from "../pages/CreateCollection";
 
 export const ReactRoutes = () => {
     return(
@@ -9,6 +10,7 @@ export const ReactRoutes = () => {
             <Route path="/" element={<HeroSection/>}/>
             <Route path="/create" element={<CreateNFTS/>}/>
             <Route path="/create/single" element={<CreateSingleNFT/>}/>
+            <Route path="/create/collection" element={<CreateCollection/>}/>
         </Routes>
     )
 }
